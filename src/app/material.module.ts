c2d15b2423071d3fcd4c7842baf1b9f1a56b848c
avinash <avibing@gmail.com>
2021-11-10 20:25:09 +0530

    Added HomeComponent, navigation component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatButtonModule} from '@angular/material/button';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
+
 
 @NgModule({
  declarations: [],
- imports: [MatBadgeModule, MatButtonModule],
- exports: [MatBadgeModule, MatButtonModule],
+ imports: [MatBadgeModule, MatButtonModule, MatNativeDateModule, HttpClientModule, 
+  MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule],
+ exports: [MatBadgeModule, MatButtonModule, MatNativeDateModule, HttpClientModule,
+  MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule],
  providers: [],
  bootstrap: []
 })
